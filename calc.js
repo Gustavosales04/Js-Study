85252c6214c0022d34c073d12254946e0b91e608
Gustavo Sales <gustavosales.mends@gmail.com>
2024-11-11 11:47:21 -0300

    modulo

---
+++ calc.js
@@ -0,0 +1,12 @@
+var SomaF = require("./soma");
+var SubF = require("./sub");
+var MultF = require("./mult");
+var DivF = require("./div");
+
+var A = 100
+var B = 50
+
+console.log(SomaF(A, B));
+console.log(SubF(A, B));
+console.log(MultF(A, B));
+console.log(DivF(A, B));
