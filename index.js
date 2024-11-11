bc596e8923989d3070c0b42ec541367f9d376c36
Gustavo Sales <gustavosales.mends@gmail.com>
2024-11-11 13:59:42 -0300

    parametros

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ app.get("/blog", function(req, res){
     res.send("Meu blog!");
 });
 
+app.get("/ola/:cargo/:nome/:color", function(req,res){
+    res.send("<h1>Olá "+req.params.nome+"</h1>"+"<h2>Seu cargo é: "+req.params.cargo+"</h2>"+"<h3>Sua cor é: "+req.params.color+"<h3>");
+})
+
 app.listen(8181, function(){
     console.log("Servidor Ativo!");
 });
